bc2a0ccb3090f76ae47aa34ba9ec699889c5be13
Arvil Meña <arvilmena@gmail.com>
2025-04-26 21:58:23 +0800

    biome fix all

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      <h1 className="text-center text-2xl font-bold border border-red-700 p-10">
-        Hello World!
-      </h1>
-    </main>
-  );
+	return (
+		<main className="flex flex-col items-center justify-center min-h-screen p-24">
+			<h1 className="text-center text-2xl font-bold border border-red-700 p-10">
+				Hello World!
+			</h1>
+		</main>
+	);
 }
